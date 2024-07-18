fbbaef785473d26e7b1b35ffa82fae88834f8f2f
Areeba Qadeer <reebz2424@gmail.com>
2024-07-18 17:17:41 +0500

    Add files via upload

---
+++ lecture13_matchStatement.py
@@ -0,0 +1,17 @@
+value = input("Enter value betwen 0 to 5")
+match value:
+    
+    case 0:
+        print("value is 0")
+    case 1:
+        print("value is 1 ")
+    case 2 :
+        print("value is 2")
+    case 3 :
+        print("value is 3")
+    case 4:
+        print("value is 4")
+    case 5 :
+        print("value is 5")
+    case _:
+        print ("false")
